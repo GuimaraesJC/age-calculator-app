049e58e52eb461c21b64c61d1a10849189322061
Jean Carlos Costa <guimaraes.jean95@gmail.com>
2023-04-16 10:43:24 -0300

    Configures theme

--- src/styles/global.js
+++ src/styles/global.js
@@ -36,6 +36,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     width: 100vw;
     height: 100vh;
+    background-color: ${({ theme }) => theme.colors.offWhite};
   }
 `;
 
---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+
+    white: 'hsl(0, 0%, 100%)',
+    offWhite: 'hsl(0, 0%, 94%)',
+    lightGrey: 'hsl(0, 0%, 86%)',
+    smokeyGrey: 'hsl(0, 1%, 44%)',
+    offBlack: 'hsl(0, 0%, 8%)',
+    purple: 'hsl(259, 100%, 65%)',
+    red: 'hsl(0, 100%, 67%)'
+  }
+};
+
+export default theme;
