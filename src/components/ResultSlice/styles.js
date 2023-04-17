44408966b6386456de9e8739fe47f34ace4bd3f4
Jean Carlos Costa <guimaraes.jean95@gmail.com>
2023-04-16 22:36:51 -0300

    Fixes letter spacing and height

--- src/components/ResultSlice/styles.js
+++ src/components/ResultSlice/styles.js
@@ -4,7 +4,8 @@ export const Paragraph = styled.p`
   font-size: 6.5rem;
   font-weight: 800;
   color: ${({ theme }) => theme.colors.offBlack};
-  line-height: 110%;
+  line-height: 7.125rem;
+  letter-spacing: -0.02rem;
 `;
 
 export const Span = styled.span`
